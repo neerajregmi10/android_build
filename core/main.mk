03f12f922b6015fadde20f356b56c744df364fb0
Pawit Pornkitprasan <p.pawit@gmail.com>
2012-01-28 09:05:16 +0700

    Disable dataroaming by default
    The property is removed so that the property in vendor/xtended
    can take effect.

--- core/main.mk
+++ core/main.mk
@@ -374,7 +374,7 @@ ADDITIONAL_BUILD_PROPERTIES += net.bt.name=Android
 
 # ------------------------------------------------------------
 # Include vendor specific additions to build properties
--include vendor/colt/build/core/main.mk
+-include vendor/legion/build/core/main.mk
 
 # ------------------------------------------------------------
 # Define a function that, given a list of module tags, returns
